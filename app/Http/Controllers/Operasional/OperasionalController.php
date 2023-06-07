b7d6fe61771b50558b01b449d104d283c5ab9d25
IT Kiosnet <indra@gmail.com>
2023-06-07 13:53:23 +0700

    Operasional

---
+++ app/Http/Controllers/Operasional/OperasionalController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Operasional;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OperasionalController extends Controller
+{
+    //
+}
